8141e14c0297c6b49ad5104a75f3cd1e1f373dbb
Agnilonda Pakou <pagnilonda@gmail.com>
2025-02-25 22:24:20 +0100

    Using structs instead of mappings

--- contracts/LockedMultySigWallet.sol
+++ contracts/LockedMultySigWallet.sol
@@ -3,10 +3,20 @@ pragma solidity 0.8.28;
 
 contract LockedMultySigWallet {
 
-    uint8 public totalApprove;
-    mapping(address => uint256) public funds;
-    mapping(address => bool) public approvals;
+    
     mapping(address => bool) public owners;
+    uint256 public transactionsCount;
+
+    struct Transaction {
+        address owner;
+        uint256 lockTime;
+        uint256 amount;
+        bool isValidated;
+        uint8 totalApprove;
+        // mapping(address => bool) approvals;
+    }
+
+    mapping(uint256 => Transaction) public transactions;
 
     error NotOwner();
     error InvalidAddress();
@@ -14,39 +24,67 @@ contract LockedMultySigWallet {
     error AlreadyApproved();
     error InsufficientApprovements();
     error UnactivatedOwner();
+    error InvalidOwner();
+    error TransactionIsLocked();
 
-    modifier OnlyOwners {
-        if(!owners[msg.sender]) revert NotOwner();
+    modifier OnlyOwners() {
+        require(owners[msg.sender] = true, "Only valid owner can performe this action.");
+        _;
+    }
+
+    constructor() {
+        owners[msg.sender] = true;
     }
     
-    function deposit(uint256 amount) external returns (bool) OnlyOwners() {
+    function deposit(uint256 amount) external OnlyOwners returns (bool) {
         if(msg.sender == address(0)) revert InvalidAddress();
         if(amount <= 0) revert InvalidAmount();
-        if(totalApprove < 3) revert InsufficientApprovements();
-        if(owners[msg.sneder] == false) revert UnactivatedOwner();
+        if(owners[msg.sender] == false) revert UnactivatedOwner();
 
-        funds[msg.sender] += amount;
-        return true
+        Transaction memory transaction = Transaction(msg.sender, block.timestamp + 120, amount, false, 0);
+        transactionsCount += 1;
+
+        transactions[transactionsCount] = transaction;
+
+        return true;
     }
 
-    function approve() external returns (bool) OnlyOwners() {
-        if(msg.sender == address(0)) revert InvalidAddress();
-        if(approvals[msg.sender] == true) revert AlreadyApproved();
-        if(owners[msg.sneder] == false) revert UnactivatedOwner();
+    function approveTransaction(uint256 transactionId) external OnlyOwners returns (bool) {
+        if(msg.sender == address(0)) revert InvalidOwner();
+        if(transactions[transactionId].lockTime <= block.timestamp) revert TransactionIsLocked();
+        
+        transactions[transactionId].isValidated = true;
 
-        totalApprove++;
-        approvals[msg.sender] = true;
-        return true
+        return true;
     }
 
-    function addOwner(address owner) external returns (bool) OnlyOwners() {
+    function rejectTransaction(uint256 transactionId) external OnlyOwners returns (bool) {
+        if(msg.sender == address(0)) revert InvalidOwner();
+        if(transactions[transactionId].lockTime <= block.timestamp) revert TransactionIsLocked();
+        
+        transactions[transactionId].isValidated = false;
+        
+        return true;
+    }
+
+    function approveApproval(uint256 transactionId) external OnlyOwners returns (bool) {
+        if(msg.sender == address(0)) revert InvalidOwner();
+        if(transactions[transactionId].lockTime <= block.timestamp) revert TransactionIsLocked();
+        if(owners[msg.sender] != true) revert InvalidOwner();
+
+        transactions[transactionId].totalApprove++;
+
+        return true;
+    }
+
+    function addOwner(address owner) external OnlyOwners returns (bool) {
         if(msg.sender == address(0)) revert InvalidAddress();
 
         owners[owner] = true;
         return true;
     }
 
-    function removeOwner(address owner) external returns (bool) OnlyOwners() {
+    function removeOwner(address owner) external OnlyOwners returns (bool) {
         if(msg.sender == address(0)) revert InvalidAddress();
 
         owners[owner] = false;
